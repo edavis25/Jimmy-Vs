12885ce230dd527f594a186b835e78a9251c0008
Eric Davis <davis.25811@gmail.com>
2018-06-27 00:16:44 -0400

    Updated event email template

--- application/views/email/event_request.php
+++ application/views/email/event_request.php
@@ -1,4 +1,24 @@
 <!DOCTYPE html>
 <html>
-    <body>Test</body>
+    <body>
+        <div>
+            <h3>
+                Someone has submitted a request to book an event!
+            </h3>
+            <ul>
+                <li>
+                    <strong>Name: </strong><?= isset($name) ? $name : 'No name given' ?>
+                </li>
+                <li>
+                    <strong>Email: </strong><?= isset($email) ? $email : 'No email given' ?>
+                </li>
+                <li>
+                    <strong>Phone: </strong><?= isset($phone) ? $phone : 'No phone number given' ?>
+                </li>
+                <li>
+                    <strong>Details: </strong><?= isset($details) ? $details : 'No details given' ?>
+                </li>
+            </ul>
+        </div>
+    </body>
 </html>
