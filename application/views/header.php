392b768f64a8346f0290de24820d129171b3ae0b
Eric Davis <davis.25811@gmail.com>
2018-06-26 22:03:20 -0400

    Fixed font awesome cdn

--- application/views/header.php
+++ application/views/header.php
@@ -22,7 +22,7 @@
         <link href="<?php echo base_url('vendor/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
 
         <!-- Custom Font CDNs -->
-        <link href="https://opensource.keycdn.com/fontawesome/4.7.0/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
         <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
         <link href="https://fonts.googleapis.com/css?family=Caesar+Dressing" rel="stylesheet"> 
